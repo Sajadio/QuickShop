fc3f3a652cf761fa33340a62f3a8446905f01921
Sajadio <sjd04969@gmail.com>
2023-06-08 16:23:33 +0300

    add colors

--- app/src/main/java/com/sajjadio/quickshop/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/sajjadio/quickshop/presentation/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val SecondaryColor = Color(0xFFFFBEA6)
+val AccentColor = Color(0xFF21DAA3)
+val PrimaryTextAndIconColor = Color(0xFF1F222B)
+val SecondaryTextColor = Color(0xFFBBBBBB)
+val TextInputFiledColor = Color(0xFFF9F9F9)
+val BaseColor = Color(0xFFFFFFFF)
