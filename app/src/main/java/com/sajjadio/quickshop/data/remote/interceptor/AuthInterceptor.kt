ba3addf09026c443874f34aed965ad9bbc7f8ea4
Sajadio <sjd04969@gmail.com>
2023-06-15 18:32:27 +0300

    add AuthInterceptor

---
+++ app/src/main/java/com/sajjadio/quickshop/data/remote/interceptor/AuthInterceptor.kt
@@ -0,0 +1,18 @@
+package com.sajjadio.quickshop.data.remote.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AuthInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        val httpUrl = request.url.newBuilder().build()
+        request = request.newBuilder().url(httpUrl).build()
+        return chain.proceed(request)
+    }
+
+}
