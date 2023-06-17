96c8e4ee10f5a0156a77c84b52bc0b7794880eb6
Sajadio <sjd04969@gmail.com>
2023-06-17 19:32:08 +0300

    add debounce to improve UX and decrease requests in search screen architecture.

--- app/src/main/java/com/sajjadio/quickshop/presentation/screen/search/SearchVewModel.kt
+++ app/src/main/java/com/sajjadio/quickshop/presentation/screen/search/SearchVewModel.kt
@@ -1,6 +1,5 @@
 package com.sajjadio.quickshop.presentation.screen.search
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,8 +8,10 @@ import com.sajjadio.quickshop.domain.repository.ShopRepository
 import com.sajjadio.quickshop.domain.utils.Resource
 import com.sajjadio.quickshop.presentation.screen.common.ProductUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,31 +27,36 @@ class SearchVewModel @Inject constructor(
 
     fun setSearchQuery(query: String) {
         searchQuery.value = query
-        loadProductData(query)
+        getProductsByName(query)
     }
 
-    private fun loadProductData(query: String) {
+    @OptIn(FlowPreview::class)
+    private fun getProductsByName(query: String) {
+        _state.update { it.copy(isLoading = true) }
         viewModelScope.launch {
-            repository.getProducts().collect { resource ->
-                when (resource) {
-                    Resource.Loading -> _state.update { it.copy(isLoading = true) }
-                    is Resource.Success -> {
-                        getProductsByName(query, resource.data)
-                    }
+            repository
+                .getProducts()
+                .debounce(500L)
+                .collect { resource ->
+                    when (resource) {
+                        Resource.Loading -> _state.update { it.copy(isLoading = true) }
+                        is Resource.Success -> {
+                            checkIfQueryEquuleusProductTitle(query, resource.data)
+                        }
 
-                    is Resource.Error -> _state.update {
-                        it.copy(
-                            isLoading = false,
-                            error = resource.errorMessage.toString()
-                        )
+                        is Resource.Error -> _state.update {
+                            it.copy(
+                                isLoading = false,
+                                error = resource.errorMessage.toString()
+                            )
+                        }
                     }
                 }
-            }
         }
 
     }
 
-    private fun getProductsByName(query: String, data: List<Product>?) {
+    private fun checkIfQueryEquuleusProductTitle(query: String, data: List<Product>?) {
         data?.filter { product ->
             product.title.contains(query, ignoreCase = true)
         }?.let { products ->
